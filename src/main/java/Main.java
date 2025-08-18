fc0242831b57788452c1dc1cbfca5129e7f2e7c3
Valerii Tilkov <vatilkov@gmail.com>
2025-08-18 14:26:18 +0300

    Задание по теме "Как компилировать и запускать Java" 2) Написав в строку и нажав кнопку <Enter> (условие задачи)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args) {
-        System.out.println(("Введите текст и нажмите <Enter>:"+args[0]));
-        //String text = new Scanner(System.in).nextLine();
-        System.out.println(args[0]);
-        System.out.println("Длина текста: " + args[0].length());
+        System.out.println(("Введите текст и нажмите <Enter>:"));
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Длина текста: " + text.length());
     }
 }
